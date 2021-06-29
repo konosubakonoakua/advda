1ac8a18d6915abb6f0f3d57ad16e8629224fc79c
konosubakonoakua <konosubaakua@163.com>
2021-06-29 15:42:03 +0800

    :bug: 解决跨域问题

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import sys
 sys.path.insert(0, "../")
 
 # Imports
+from fastapi.middleware.cors import CORSMiddleware # CORS
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -10,6 +11,7 @@ from starlette.responses import FileResponse
 from typing import Optional
 import uvicorn
 
+from backend import cfg
 from backend import router_yolov5
 
 # Load the model
@@ -18,6 +20,23 @@ from backend import router_yolov5
 
 # Define the FastAPI app
 app = FastAPI()
+
+# CORS
+origins = [
+    # "http://localhost.tiangolo.com",
+    # "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:23333",
+    "http://localhost:4000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Add routers
 app.include_router(router_yolov5.router)
@@ -38,12 +57,12 @@ async def root_route():
 async def get_img_file(filename: Optional[str] = None):
     if filename is not None:
         # img: PIL.Image.Image = Image.open(f"web/imgs/{filename}")
-        return FileResponse(f"imgs/{filename}", media_type="image/png")
+        return FileResponse(f"imgs/{filename}", media_type="image/"+filename.split(".")[-1])
     else:
         raise HTTPException(status_code=500, detail="wrong file name")
 
 
 if __name__ == "__main__":
-    for p in sys.path:
-        print(p)
-    uvicorn.run(app=app, host="localhost", port=8888)
+    # for p in sys.path:
+    #     print(p)
+    uvicorn.run(app=app, host=cfg.SERVER_HOST, port=cfg.SERVER_PORT)
