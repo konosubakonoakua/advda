5c57619bf93dec2dc696b70188d4427e803a998d
konosubakonoakua <konosubaakua@163.com>
2021-06-30 11:30:38 +0800

    :nut_and_bolt: 添加 utils 文件夹 和 imagenet labels

---
+++ backend/utils/imagenet.py
@@ -0,0 +1,18 @@
+import json
+import os
+from typing import List
+def get_imagenet_classes() -> dict:
+    # print(os.path.abspath("."))
+    # print(os.path.curdir)
+    # 坑壁啊，居然默认是在整个项目的根目录下
+    with open("./utils/imagenet_classes.json", 'r') as f:
+        mapping = json.loads(f.read())
+    return mapping
+
+def get_imagenet_classes_zh() -> List[str]:
+    # print(os.path.abspath("."))
+    # print(os.path.curdir)
+    # 坑壁啊，居然默认是在整个项目的根目录下
+    with open("./utils/imagenet_classes_zh.json", 'r', encoding='utf8') as f:
+        mapping = json.loads(f.read())
+    return mapping
