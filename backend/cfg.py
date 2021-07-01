85f038e19a314e4f33d0f690842c7baf32e830ea
konosubakonoakua <konosubaakua@163.com>
2021-07-01 22:31:12 +0800

    :full_moon_with_face: 修改配置文件

--- backend/cfg.py
+++ backend/cfg.py
@@ -1,5 +1,5 @@
 # MODEL_PATH_YOLO = "checkpoints/yolov5s.pt"
 MODEL_PATH_YOLO = "checkpoints/yolov5x6.pt"
 MODEL_REPO_YOLO = 'yolov5'
-SERVER_PORT = '23333'
+SERVER_PORT = 23333
 SERVER_HOST = 'localhost'
--- backend/deploy.bat
+++ backend/deploy.bat
@@ -1,1 +1,2 @@
-uvicorn main:app --port 23333
+@REM uvicorn main:app --port 23333 --reload --use-colors
+python -m main
